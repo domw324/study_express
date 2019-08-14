3ba6b7369be3f4b54a270a514b648d3f71b98edb
justchange <domw324@gmail.com>
2019-08-14 10:43:32 +0900

    middle ware 실습 : body-parser, compression, static file / 에러처리 실습

--- main.js
+++ main.js
@@ -1,37 +1,56 @@
 const express = require('express')
 const app = express()
-var path = require('path');
-var fs = require('fs');
-var qs = require('querystring');
+
+var path = require('path'); // parsing the paths
+var fs = require('fs'); // controling the file system
+var qs = require('querystring'); // parsing the data on request
+var sanitizeHtml = require('sanitize-html'); // 사용자 input에서 악성 스크립트 필터링
+var bodyParser = require('body-parser'); // middleware : 
+var compression = require('compression'); // middleware : 전송 데이터 압축
 var template = require('./lib/template.js');
-var sanitizeHtml = require('sanitize-html');
+
+app.use(express.static('public')); // public directory에서 static file 검색 & 사용
+app.use(bodyParser.urlencoded({ extended: false})); // body-parser 사용
+app.use(compression()); // compression 사용
+// custom middleware : directory filelist 반환. get 방식 요청의 모든 경로에서 실행
+app.get('*', function(request, response, next){
+  fs.readdir('./data', function(error, filelist){
+    request.list = filelist;
+    next();
+  });
+});
 
 //route, routing
 // app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/', function(request, response) {
-  fs.readdir('./data', function(error, filelist){
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = template.list(filelist);
-    var html = template.HTML(title, list,
-      `<h2>${title}</h2>${description}`,
-      `<a href="/create">create</a>`
-    );
-    response.send(html);
-  });
+  // fs.readdir('./data', function(error, filelist){
+  // });
+  var title = 'Welcome';
+  var description = 'Hello, Node.js';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list,
+    `
+    <h2>${title}</h2>${description}
+    <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
+    `
+    ,
+    `<a href="/create">create</a>`
+  );
+  response.send(html);
 });
 
-app.get('/page/:pageId', function(request, response) {
-  // response.send(request.params);
-  fs.readdir('./data', function(error, filelist){
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+app.get('/page/:pageId', function(request, response, next) {
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+    if(err){
+      next(err);
+    } else{
       var title = request.params.pageId;
       var sanitizedTitle = sanitizeHtml(title);
       var sanitizedDescription = sanitizeHtml(description, {
         allowedTags:['h1']
       });
-      var list = template.list(filelist);
+      var list = template.list(request.list);
       var html = template.HTML(sanitizedTitle, list,
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
         ` <a href="/create">create</a>
@@ -42,33 +61,33 @@ app.get('/page/:pageId', function(request, response) {
           </form>`
       );
       response.send(html);
-    });
+    }
   });
 });
 
 app.get('/create', function(request, response) {
-  fs.readdir('./data', function(error, filelist){
-    var title = 'WEB - create';
-    var list = template.list(filelist);
-    var html = template.HTML(title, list, `
-      <form action="/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `, '');
-    response.send(html);
-  });
+  var title = 'WEB - create';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list, `
+    <form action="/create_process" method="post">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p>
+        <textarea name="description" placeholder="description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
+  `, '');
+  response.send(html);
 });
 
 app.post('/create_process', function(request, response) {
+  /*
+  // body-parser 사용 전 코드. (querystring 사용)
   var body = '';
   request.on('data', function(data){
-      body = body + data;
+    body = body + data;
   });
   request.on('end', function(){
     var post = qs.parse(body);
@@ -78,65 +97,69 @@ app.post('/create_process', function(request, response) {
       response.redirect(`/page/${title}`);
     })
   });
+  */
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+    response.redirect(`/page/${title}`);
+  })
 })
 
 app.get('/update/:pageId', function(request, response) {
-  fs.readdir('./data', function(error, filelist){
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      var title = request.params.pageId;
-      var list = template.list(filelist);
-      var html = template.HTML(title, list,
-        `
-        <form action="/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-        `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-      );
-      response.send(html);
-    });
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+    var title = request.params.pageId;
+    var list = template.list(request.list);
+    var html = template.HTML(title, list,
+      `
+      <form action="/update_process" method="post">
+        <input type="hidden" name="id" value="${title}">
+        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+        <p>
+          <textarea name="description" placeholder="description">${description}</textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+      `,
+      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+    );
+    response.send(html);
   });
 })
 
 app.post('/update_process', function(request, response) {
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-    var post = qs.parse(body);
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function(error){
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        response.redirect(`/page/${title}`);
-      })
-    });
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function(error){
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+      response.redirect(`/page/${title}`);
+    })
   });
 });
 
 app.post('/delete_process', function(request, response) {
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var id = post.id;
-      var filteredId = path.parse(id).base;
-      fs.unlink(`data/${filteredId}`, function(error){
-        response.redirect('/');
-      })
-  });
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function(error){
+    response.redirect('/');
+  })
+})
+
+// 404 : Not Founded Error
+app.use(function(req, res, next) {
+  res.status(404).setDefaultEncoding('Sorry cant find that!');
+});
+
+// error 처리
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 })
 
 app.listen(3000, function() {
