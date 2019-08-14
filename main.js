79f5902220e4de3de7e9faf9d6ca77b23091dee8
justchange <domw324@gmail.com>
2019-08-14 11:53:10 +0900

    router 사용. 코드 모듈화

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const app = express()
 
-var path = require('path'); // parsing the paths
 var fs = require('fs'); // controling the file system
-var qs = require('querystring'); // parsing the data on request
-var sanitizeHtml = require('sanitize-html'); // 사용자 input에서 악성 스크립트 필터링
+// var qs = require('querystring'); // parsing the data on request
 var bodyParser = require('body-parser'); // middleware : 
 var compression = require('compression'); // middleware : 전송 데이터 압축
 var template = require('./lib/template.js');
+var topicRouter = require('./routes/topic');
 
 app.use(express.static('public')); // public directory에서 static file 검색 & 사용
 app.use(bodyParser.urlencoded({ extended: false})); // body-parser 사용
@@ -19,7 +18,6 @@ app.get('*', function(request, response, next){
     next();
   });
 });
-
 //route, routing
 // app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/', function(request, response) {
@@ -45,122 +43,13 @@ app.get('/', function(request, response) {
     <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
     `
     ,
-    `<a href="/create">create</a>`
+    `<a href="/topic/create">create</a>`
   );
   response.send(html);
 });
 
-app.get('/page/:pageId', function(request, response, next) {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-    if(err){
-      next(err);
-    } else{
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags:['h1']
-      });
-      var list = template.list(request.list);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action="/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>`
-      );
-      response.send(html);
-    }
-  });
-});
-
-app.get('/create', function(request, response) {
-  var title = 'WEB - create';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list, `
-    <form action="/create_process" method="post">
-      <p><input type="text" name="title" placeholder="title"></p>
-      <p>
-        <textarea name="description" placeholder="description"></textarea>
-      </p>
-      <p>
-        <input type="submit">
-      </p>
-    </form>
-  `, '');
-  response.send(html);
-});
-
-app.post('/create_process', function(request, response) {
-  /*
-  // body-parser 사용 전 코드. (querystring 사용)
-  var body = '';
-  request.on('data', function(data){
-    body = body + data;
-  });
-  request.on('end', function(){
-    var post = qs.parse(body);
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-      response.redirect(`/page/${title}`);
-    })
-  });
-  */
-  var post = request.body;
-  var title = post.title;
-  var description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-    response.redirect(`/page/${title}`);
-  })
-})
-
-app.get('/update/:pageId', function(request, response) {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-    var title = request.params.pageId;
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
-      `
-      <form action="/update_process" method="post">
-        <input type="hidden" name="id" value="${title}">
-        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-        <p>
-          <textarea name="description" placeholder="description">${description}</textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-      `,
-      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-    );
-    response.send(html);
-  });
-})
-
-app.post('/update_process', function(request, response) {
-  var post = request.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function(error){
-    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-      response.redirect(`/page/${title}`);
-    })
-  });
-});
-
-app.post('/delete_process', function(request, response) {
-  var post = request.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function(error){
-    response.redirect('/');
-  })
-})
+// module : /topic 경로로 들어오는 모든 요청은 topicRouter가 처리
+app.use('/topic', topicRouter);
 
 // 404 : Not Founded Error
 app.use(function(req, res, next) {
