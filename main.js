8597f30a158d0dd41526d82b221000baa0b3773c
justchange <domw324@gmail.com>
2019-08-14 11:22:19 +0900

    주석 추가

--- main.js
+++ main.js
@@ -23,8 +23,19 @@ app.get('*', function(request, response, next){
 //route, routing
 // app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/', function(request, response) {
-  // fs.readdir('./data', function(error, filelist){
-  // });
+  /*
+  // custom middleware 사용 전 코드
+  fs.readdir('./data', function(error, filelist){
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+    var list = template.list(request.list);
+    var html = template.HTML(title, list,
+      `<h2>${title}</h2>${description}`,
+      `<a href="/create">create</a>`
+    );
+    response.send(html);
+  });
+  */
   var title = 'Welcome';
   var description = 'Hello, Node.js';
   var list = template.list(request.list);
